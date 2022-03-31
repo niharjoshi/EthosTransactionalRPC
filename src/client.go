17ca17b244c9bc5a182ecc5e464c302144d92c5b
niharjoshi <nsj0596@gmail.com>
2022-03-30 21:04:21 -0500

    Added client base
    Signed-off-by: niharjoshi <nsj0596@gmail.com>

--- src/client.go
+++ src/client.go
@@ -1,1 +1,58 @@
 package src
+
+import (
+	"ethos/altEthos"
+	"log"
+)
+
+func init() {
+	CreateAccountRPCReply(createAccountRPCReply)
+	GetBalanceRPCReply(getBalanceRPCReply)
+	TransferMoneyRPCReply(transferMoneyRPCReply)
+}
+
+func createAccountRPCReply(accountHolderName string, startingBalance float64) {
+
+}
+
+func getBalanceRPCReply(accountHolderName string) {
+
+}
+
+func transferMoneyRPCReply(sourceAccountHolderName string, destinationAccountHolderName string) {
+
+}
+
+func menu() {
+
+	for {
+
+		EthosSTDOUT("Options:\n")
+		EthosSTDOUT("1. Create account\n")
+		EthosSTDOUT("2. Get balance\n")
+		EthosSTDOUT("3. Transfer money\n")
+		EthosSTDOUT("4. Exit\n")
+
+		var input = EthosSTDIN()
+
+		if input == "1\n" {
+
+		} else if input == "2\n" {
+
+		} else if input == "3\n" {
+
+		} else if input == "4\n" {
+			break
+		} else {
+			EthosSTDOUT("Wrong input, try again.\n")
+		}
+
+	}
+}
+
+func main() {
+	altEthos.LogToDirectory("log/client")
+	log.Printf("Starting RPC client\n")
+	menu()
+	log.Printf("Shutting down RPC client\n")
+}
